2bc1c55f3900d58b5b493ff85ef5bf4939030c6f
hiroki4455 <tue0_0125@yahoo.co.jp>
2020-04-02 15:58:40 +0900

    バリデーションに関する５つのテスト

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,15 @@
 FactoryBot.define do
   factory :task do
     title { "hoge" }
+    # sequence(:title) { |n| "hoge#{n}" }
     status { "doing" }
+    initialize_with { Task.find_or_create_by(title: title)}
   end
 end
+
+# FactoryBot.define do
+# factory :task, class: Task do
+# title {‘hoge’}
+# status {:doing}
+# end
+# end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,8 +4,36 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe '#create' do
     let(:task){ build :task}
-    it "has a valid factory" do
+    it 'is valid with all attributes' do
       expect(task).to be_valid
+    end
+
+    it 'is invalid without title' do
+      @task = Task.new(title: nil)
+      expect(@task.valid?).to eq(false)
+    end
+
+    it 'is invalid without status' do
+      @task = Task.new(status: nil)
+      expect(@task.valid?).to eq(false)
+    end
+
+    it 'is valid with another title' do
+      @task = Task.create(title: 'hoge2', status: 'doing')
+      expect(task).to be_valid
+    end
+
+    it "is invalid with a duplicate title" do
+      Task.create(
+        title: 'hoge',
+        status: 'doing'
+      )
+      @task = Task.new(
+        title: 'hoge',
+        status: 'doing'
+      )
+      @task.valid?
+      expect(@task.valid?).to eq(false)
     end
   end
 end
